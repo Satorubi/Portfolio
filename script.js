c5138a679f120d6eb23dcb0bdb3143fc2529a19a
Satorubi <136783561+Satorubi@users.noreply.github.com>
2024-03-15 16:26:39 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.Name', {
+    strings: ['Satorubi', 'a Student', 'a Aspiring Web Developer'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+});
